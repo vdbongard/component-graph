3dadae827408874482652ea6a296c5404c42e87f
vdbongard <daniel@vdbongard.com>
2019-12-23 00:24:26 +0100

    Filter files to only include supported extensions and without test files

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,6 +6,7 @@ import { BehaviorSubject } from 'rxjs';
 import { Graph, Link, Node } from '../interfaces';
 import { reactMethods } from '../constants/special-methods';
 import { FileWithPath } from '../helper/getFilesAsync';
+import { supportedExtensions } from '../constants/fileExtensions';
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +21,18 @@ export class DataService {
 
   async setFiles(files: FileWithPath[]) {
     console.log('setFiles: ', files);
-    this.setFile(files[0].file);
+    const componentFiles = files.filter(file => {
+      const pathParts = file.path.split('.');
+      return (
+        pathParts.length >= 2 &&
+        pathParts[pathParts.length - 2] !== 'test' &&
+        supportedExtensions.includes(pathParts[pathParts.length - 1])
+      );
+    });
+    console.log('componentFiles: ', componentFiles);
+    if (componentFiles) {
+      this.setFile(componentFiles[0].file);
+    }
   }
 
   async setFile(file: File) {
