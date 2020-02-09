7abf7f91dfcf796ace26e049eac6b3aa9679b158
vdbongard <daniel@vdbongard.com>
2020-02-09 22:34:09 +0100

    Fix quality metric not showing progress on first selection (due to threshold being not set yet)

--- src/app/components/quality-metric/quality-metric.component.ts
+++ src/app/components/quality-metric/quality-metric.component.ts
@@ -8,16 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class QualityMetricComponent implements OnInit {
   @Input() name;
   @Input() threshold;
-  @Input() set value(value: number) {
-    this.valueInternal = value;
-    this.progressValue = (value / this.threshold) * 100;
-  }
-  get value(): number {
-    return this.valueInternal;
-  }
-
-  private valueInternal: number;
-  progressValue: number;
+  @Input() value;
 
   constructor() {}
 
